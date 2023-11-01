83cd0a81ababeb4b776b13856ba106efe3ca7312
Leonardo José Nunes <leonardo.ferax@gmail.com>
2023-11-01 14:51:43 -0300

    Building the Layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 🛍 </h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for you 👓 </h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>
+        🛍 You have X items on your list, and you already
+        packed x (x%)
+      </em>
+    </footer>
+  );
 }
 
 export default App;
