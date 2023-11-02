042607682fff3ef33cdec0b163ff89252970827f
Leonardo José Nunes <leonardo.ferax@gmail.com>
2023-11-02 13:13:55 -0300

    Deleting an Item: More Child-to-Parent Communication!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,5 @@
 import { useState } from "react";
 
-// const initialItems = [
-//   {
-//     id: 1,
-//     description: "Passports",
-//     quantity: 2,
-//     packed: false
-//   },
-//   {
-//     id: 2,
-//     description: "Socks",
-//     quantity: 12,
-//     packed: true
-//   },
-//   {
-//     id: 3,
-//     description: "Charger",
-//     quantity: 1,
-//     packed: false
-//   }
-// ];
-
 function App() {
   const [items, setItems] = useState([]);
 
@@ -28,11 +7,19 @@ function App() {
     setItems(items => [...items, item]);
   }
 
+  function handleDeleteItem(id) {
+    // console.log(id);
+    setItems(items => items.filter(item => item.id !== id));
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+      />
       <Stats />
     </div>
   );
@@ -59,7 +46,6 @@ function Form({ onAddItems }) {
       id: Date.now()
     };
 
-    // console.log(newItem);
     onAddItems(newItem);
 
     setDescription("");
@@ -74,7 +60,7 @@ function Form({ onAddItems }) {
       <h3>What do you need for you 👓 </h3>
 
       <select
-        name={quantity}
+        name="quantity"
         value={quantity}
         onChange={e => setQuantity(Number(e.target.value))}
       >
@@ -99,19 +85,23 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul>
         {items.map(item => (
-          <Item item={item} key={item.id} />
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItem={onDeleteItem}
+          />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span
@@ -122,7 +112,9 @@ function Item({ item }) {
         }
       >
         {item.quantity} {item.description}
-        <button>❌</button>
+        <button onClick={e => onDeleteItem(item.id)}>
+          ❌
+        </button>
       </span>
     </li>
   );
