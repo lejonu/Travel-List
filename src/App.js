21a06e6b230c44c19f313857d63f4ec2b67e2f4b
Leonardo José Nunes <leonardo.ferax@gmail.com>
2023-11-02 18:58:28 -0300

    Sorting Items

--- src/App.js
+++ src/App.js
@@ -101,10 +101,28 @@ function PackingList({
   onDeleteItem,
   onToggleItem
 }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) =>
+        a.description.localeCompare(b.description)
+      );
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {items.map(item => (
+        {sortedItems.map(item => (
           <Item
             item={item}
             key={item.id}
@@ -113,6 +131,22 @@ function PackingList({
           />
         ))}
       </ul>
+      <div className="actions">
+        <select
+          name=""
+          id=""
+          value={sortBy}
+          onChange={e => setSortBy(e.target.value)}
+        >
+          <option value="input">Sort by input order</option>
+          <option value="description">
+            Sort by description
+          </option>
+          <option value="packed">
+            Sort by packed status
+          </option>
+        </select>
+      </div>
     </div>
   );
 }
